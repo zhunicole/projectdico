25214d57410c060cc76cfedafa04e419777f9ec4
Nicole Zhu <nicolez@stanford.edu>
2014-05-24 14:19:37 -0700

    project dico

---
+++ ProjectDico/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  ProjectDico
+//
+//  Created by Nicole Zhu on 5/24/14.
+//  Copyright (c) 2014 ProjectDico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ ProjectDico/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  ProjectDico
+//
+//  Created by Nicole Zhu on 5/24/14.
+//  Copyright (c) 2014 ProjectDico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
